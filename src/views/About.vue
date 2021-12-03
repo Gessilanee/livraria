d8244f91a4b2262a619be5fde266d6011f771752
gessi <gessilane.i3solucoes@gmail.com>
2021-12-03 16:02:26 -0300

    Botão de envio

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,130 +1,21 @@
 <template>
-  <v-row justify="center">
-    <v-col
-      cols="12"
-      sm="10"
-      md="8"
-      lg="6"
-    >
-      <v-card ref="form" >
-        <v-card-text >
-          <v-text-field 
-            ref="name"
-            v-model="name"
-            :error-messages="errorMessages"
-            label="Digite o nome do livro"
-            required
-          ></v-text-field>
-          <v-text-field
-            ref="state"
-            v-model="state"
-            label="Digite o nome do autor "
-            required
-          ></v-text-field>
-          <v-text-field
-            ref="zip"
-            v-model="zip"
-            label="Digite a descrição"
-            required
-          ></v-text-field>
-        </v-card-text>
-        <v-divider class="mt-12"></v-divider>
-        <v-card-actions>
-          <v-btn text>
-            Cancel
-          </v-btn>
-          <v-spacer></v-spacer>
-          <v-slide-x-reverse-transition>
-            <v-tooltip
-              v-if="formHasErrors"
-              left
-            >
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  icon
-                  class="my-0"
-                  v-bind="attrs"
-                  @click="resetForm"
-                  v-on="on"
-                >
-                  <v-icon>mdi-refresh</v-icon>
-                </v-btn>
-              </template>
-              <span>Refresh form</span>
-            </v-tooltip>
-          </v-slide-x-reverse-transition>
-          <v-btn
-            color="primary"
-            text
-            @click="submit"
-          >
-            Submit
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+    <form method="POST" action="">
+    <div id="idDiv">   
+        <h5>Digite um livro:</h5>     
+        <input type="text" class="form-control" id="item" placeholder="Digite um livro" style="max-width:300px;">        
+    </div>
+    <div class="submit">
+        <v-btn text small color="primary"> Adicionar</v-btn>
+    </div>
+</form>
 </template>
 
 <script>
 
   export default {
-    data: () => ({
-      errorMessages: '',
-      name: null,
-      address: null,
-      city: null,
-      state: null,
-      zip: null,
-      country: null,
-      formHasErrors: false,
-    }),
-
-    computed: {
-      form () {
-        return {
-          name: this.name,
-          address: this.address,
-          city: this.city,
-          state: this.state,
-          zip: this.zip,
-          country: this.country,
-        }
-      },
+    return:{
+     message: "Enviar"
     },
-
-    watch: {
-      name () {
-        this.errorMessages = ''
-      },
-    },
-
-    methods: {
-      addressCheck () {
-        this.errorMessages = this.address && !this.name
-          ? `Hey! I'm required`
-          : ''
-
-        return true
-      },
-      resetForm () {
-        this.errorMessages = []
-        this.formHasErrors = false
-
-        Object.keys(this.form).forEach(f => {
-          this.$refs[f].reset()
-        })
-      },
-      submit () {
-        this.formHasErrors = false
-
-        Object.keys(this.form).forEach(f => {
-          if (!this.form[f]) this.formHasErrors = true
-
-          this.$refs[f].validate(true)
-        })
-      },
-    },
-  
-  }
+  };
+   
 </script>
