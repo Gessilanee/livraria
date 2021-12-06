2c01496e39d520b7e947638455d3dac736f02928
gessi <gessilane.i3solucoes@gmail.com>
2021-12-06 10:02:46 -0300

    form no arquivo de livros

--- src/views/Livros.vue
+++ src/views/Livros.vue
@@ -9,7 +9,22 @@
           {{livro.name}} {{livro.author}}
         </li>
       </ul>
-
+      <br>
+  <form action="" method="post" @submit.stop.prevent="send">
+    <div>
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="name"/>
+    </div>
+    <div>
+        <label for="email">Autor:</label>
+        <input type="text" id="author" name="author"/>
+    </div>
+    <div>
+        <label for="msg">Descrição:</label>
+        <textarea id="description" name="description"></textarea>
+    </div>
+    <input type="button" value="Enviar" onclick="msg()">
+</form>
   </div>
 </template>
 
